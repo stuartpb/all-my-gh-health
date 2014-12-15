f69bc935ddbcc797e29120b6f47bcd1e2648b877
Stuart P. Bentley <stuart@testtrack4.com>
2014-12-15 22:53:27 +0000

    Buh, GitHub was right, I'm such a prat

--- list-gh-cnames.js
+++ list-gh-cnames.js
@@ -1,1 +1,36 @@
 #!/usr/bin/env node
+
+var request = require("request");
+
+var username = process.argv[2];
+
+function jsonRequest(url, cb) {
+  request({
+    url: url,
+    headers: {
+      'User-Agent': 'request',
+      'Accept': 'application/vnd.github.moondragon-preview+json'
+    },
+    json: true
+  }, cb);
+}
+
+function receiveRepos(err, res, body) {
+  body.forEach(function(repo) {
+    if (!repo.fork) {
+      request('https://raw.githubusercontent.com/' +
+        repo.full_name +
+        (/^([^\/]+)\/\1\.github\.(com|io)$/.test(repo.full_name) ?
+          '/master' : '/gh-pages') +
+        '/CNAME', function (err, res, body) {
+          if (res.statusCode == 200) console.log(body.trim());
+        });
+    }
+  });
+  var nextLink = res.headers.link.match(/<([^>]*)>; rel="next"/);
+  if (nextLink) {
+    jsonRequest(nextLink[1],receiveRepos);
+  }
+}
+
+jsonRequest('https://api.github.com/users/'+username+'/repos', receiveRepos);
